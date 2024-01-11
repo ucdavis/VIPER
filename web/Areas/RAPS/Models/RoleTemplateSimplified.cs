8365a0c1a044c672dbc748da6b2edac0204388ff
Brandon <bsedwards@ucdavis.edu>
2024-01-11 15:46:31 -0800

    FIxing role template list not loading due to excess fields returned

---
+++ web/Areas/RAPS/Models/RoleTemplateSimplified.cs
@@ -0,0 +1,16 @@
+﻿using Viper.Models.RAPS;
+
+namespace Viper.Areas.RAPS.Models
+{
+    /* Streamlined object to return from the RoleTemplate REST calls */
+    public class RoleTemplateSimplified
+    {
+        public int RoleTemplateId { get; set; }
+
+        public string TemplateName { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public virtual ICollection<RoleSimplified> Roles { get; set; } = new List<RoleSimplified>();
+    }
+}
