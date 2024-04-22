a5e33be706feb8f5ee09b2452fbba115e1ef4f61
Brandon <bsedwards@ucdavis.edu>
2024-04-22 12:55:44 -0700

    Adding message

--- web/Areas/RAPS/Services/LdapService.cs
+++ web/Areas/RAPS/Services/LdapService.cs
@@ -80,7 +80,7 @@ namespace Viper.Areas.RAPS.Services
                 var de = GetRoot(true);
                 _logger.Info("LDAP DE Server: " + de?.Options?.GetCurrentServerName());
                 var ds = new DirectorySearcher(de, filter, _groupProperties, SearchScope.Subtree)
-                { PageSize = 1000, ReferralChasing = ReferralChasingOption.All };
+                    { PageSize = 1000, ReferralChasing = ReferralChasingOption.All };
 
                 SearchResultCollection results = ds.FindAll();
                 foreach (SearchResult result in results)
@@ -92,7 +92,7 @@ namespace Viper.Areas.RAPS.Services
             catch (DirectoryServicesCOMException cex)
             {
                 _logger.Error(cex);
-                _logger.Error(cex.ExtendedError);
+                _logger.Error("Extended Error: " + cex?.ExtendedErrorMessage);
             }
             catch (Exception ex)
             {
