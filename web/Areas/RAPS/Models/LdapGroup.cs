2487de1ed839d16deac8a4de20bbca8f5d4eeaed
Brandon <bsedwards@ucdavis.edu>
2024-04-26 15:55:38 -0700

    Using DirectoryServices.Protocols

--- web/Areas/RAPS/Models/LdapGroup.cs
+++ web/Areas/RAPS/Models/LdapGroup.cs
@@ -1,4 +1,5 @@
 ﻿using System.DirectoryServices;
+using System.DirectoryServices.Protocols;
 using System.Runtime.Versioning;
 
 namespace Viper.Areas.RAPS.Models
@@ -47,6 +48,31 @@ namespace Viper.Areas.RAPS.Models
 
                     }
                 }
+            }
+        }
+
+        public LdapGroup(SearchResultEntry entry)
+        {
+            foreach (DirectoryAttribute attr in entry.Attributes.Values)
+            {
+                var v = attr[0];
+                switch (attr.Name)
+                {
+                    case "sAMAccountName": SamAccountName = v.ToString(); break;
+                    case "objectGUID": ObjectGuid = (byte[])v; break;
+                    case "cn": Cn = v.ToString(); break;
+                    case "canonicalName": CanonicalName = v.ToString(); break;
+                    case "dn": Dn = v.ToString(); break;
+                    case "distinguishedName": DistinguishedName = v.ToString(); break;
+                    case "displayName": DisplayName = v.ToString(); break;
+                    case "groupType": GroupType = v.ToString(); break;
+                    case "description": Description = v.ToString(); break;
+                    case "extensionattribute1": ExtensionAttribute1 = v.ToString(); break;
+                    case "extensionattribute2": ExtensionAttribute2 = v.ToString(); break;
+                    case "extensionattribute3": ExtensionAttribute3 = v.ToString(); break;
+                    default: break;
+                }
+
             }
         }
     }
