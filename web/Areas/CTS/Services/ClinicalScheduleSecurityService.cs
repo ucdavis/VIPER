caf9ad65494154ecbc817ca0a158e26269097e6c
Brandon <bsedwards@ucdavis.edu>
2024-05-09 16:19:12 -0700

    Student clinical schedule placeholders

---
+++ web/Areas/CTS/Services/ClinicalScheduleSecurityService.cs
@@ -0,0 +1,55 @@
+﻿
+using Viper.Classes.SQLContext;
+
+namespace Viper.Areas.CTS.Services
+{
+	/// <summary>
+	/// Clinical schedule permission checking. Should probably be moved into clinical scheduler when that app is converted.
+	/// </summary>
+	public class ClinicalScheduleSecurityService
+	{
+		private static readonly string MANAGE_PERM = "SVMSecure.ClnSched.Manage";
+		private static readonly string VIEWSTD_PERM = "SVMSecure.ClnSched.ViewStdSchedules";
+		private static readonly string VIEWOWN_PERM = "SVMSecure.ClnSched.MySchedule";
+
+		private readonly RAPSContext rapsContext;
+		public ClinicalScheduleSecurityService(RAPSContext rapsContext)
+		{
+			this.rapsContext = rapsContext;
+		}
+
+		/// <summary>
+		/// Check access to student schedules with the given params
+		/// - Students can view their own schedule (if they have the MySchedule Permission)
+		/// - Students can also view the schedule for a rotation/week they are on (NOT IMPLEMENTED for CTS)
+		/// - Accommodation users can view marked students (NOT IMPLEMENTED for CTS)
+		/// - Otherwise, Managers and ViewStdSchedules can view all schedules
+		/// </summary>
+		/// <param name="classYear"></param>
+		/// <param name="mothraId"></param>
+		/// <param name="rotationId"></param>
+		/// <param name="serviceId"></param>
+		/// <param name="weekId"></param>
+		/// <param name="startDate"></param>
+		/// <param name="endDate"></param>
+		/// <returns></returns>
+		public bool CheckStudentScheduleParams(string? mothraId, int? rotationId, int? serviceId, int? weekId, DateTime? startDate, DateTime? endDate)
+		{
+			var uh = new UserHelper();
+			var user = uh.GetCurrentUser();
+			if(user != null)
+			{
+				if(uh.HasPermission(rapsContext, user, MANAGE_PERM) || uh.HasPermission(rapsContext, user, VIEWSTD_PERM))
+				{
+					return true;
+				}
+				if(mothraId != null && mothraId == user.MothraId && uh.HasPermission(rapsContext, user, VIEWOWN_PERM))
+				{
+					return true;
+				}
+				//NB: some access rules not implemented
+			}
+			return false;
+		}
+	}
+}
---
+++ web/Areas/CTS/Services/ClinicalScheduleService.cs
@@ -0,0 +1,24 @@
+﻿using Viper.Areas.CTS.Models;
+using Viper.Classes.SQLContext;
+using Viper.Models.CTS;
+
+namespace Viper.Areas.CTS.Services
+{
+	public class ClinicalScheduleService
+	{
+		private readonly VIPERContext context;
+		private readonly RAPSContext rapsContext;
+		public ClinicalScheduleService(VIPERContext context, RAPSContext rapsContext)
+		{
+			this.context = context;
+			this.rapsContext = rapsContext;	
+		}
+
+		public async Task<List<StudentSchedule>> GetStudentSchedule(int? classYear, string? mothraId, int? rotationId, int? serviceId, int? weekId, DateTime? startDate,
+			DateTime? endDate)
+		{
+
+			return new List<StudentSchedule>();
+		}
+	}
+}
