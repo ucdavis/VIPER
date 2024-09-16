ff57e3be9fdbba02724a13e8403b24120cd7bc3d
Brandon <bsedwards@ucdavis.edu>
2024-09-16 14:11:39 -0700

    Student results view

--- VueApp/src/CTS/pages/MyAssessments.vue
+++ VueApp/src/CTS/pages/MyAssessments.vue
@@ -1,58 +1,62 @@
 <script setup lang="ts">
+    import { useQuasar } from 'quasar'
     import { ref, inject } from 'vue'
     import type { Ref } from 'vue'
     import { useRoute } from 'vue-router'
     import { useUserStore } from '@/store/UserStore'
     import { useFetch } from '@/composables/ViperFetch'
-    import type { Assessment, Epa, Level } from '@/CTS/types'
+    import type { Assessment, Epa, Level, Person } from '@/CTS/types'
     import { useDateFunctions } from '@/composables/DateFunctions'
-    import {
-        Chart as ChartJS,
-        Title,
-        Tooltip,
-        Legend,
-        LineElement,
-        CategoryScale,
-        LinearScale,
-    } from 'chart.js/auto'
-    import { Line } from 'vue-chartjs'
-    import type { ChartData, Point } from 'chart.js'
+    import AssessmentBubble from '@/CTS/components/AssessmentBubble.vue'
 
-    const apiUrl = inject('apiURL')
-    const baseUrl = apiUrl + "cts/"
-    const epaAssessments = ref([]) as Ref<Assessment[]>
-    const epas = ref([]) as Ref<Epa[]>
-    const assessmentURL = baseUrl + 'assessments'
     const userStore = useUserStore()
     const { get } = useFetch()
     const { formatDate } = useDateFunctions()
+
+    const apiUrl = inject('apiURL')
+    const person = ref(null) as Ref<Person | null>
+    let studentUserId = userStore.userInfo.userId
+    const showPersonName = ref(false)
+    const epaAssessments = ref([]) as Ref<Assessment[]>
+    const epas = ref([]) as Ref<Epa[]>
     const levels = ref([]) as Ref<Level[]>
+    const loaded = ref(false)
+    const showDetails = ref([]) as Ref<boolean[]>
+    const bubbleType = ref("bubble")
 
-    function getEpas() {
-        get(baseUrl + "epas")
-            .then(r => epas.value = r.result)
-    }
+    async function load() {
+        const $q = useQuasar()
+        $q.loading.show({
+            message: "Loading",
+            delay: 250 // ms
+        })
 
-    function getLevels() {
-        get(baseUrl + "levels")
-            .then(r => levels.value = r.result)
+        const route = useRoute()
+        const params = route.query
+        if (params.student != null) {
+            let result = await get(apiUrl + "cts/permissions?access=ViewStudentAssessments&studentId=" + params.student)
+            let canAccess = result.result
+            const studentParam = parseInt(params.student.toString())
+            if (studentParam != undefined && canAccess) {
+                studentUserId = studentParam
+                showPersonName.value = true
+            }
+        }
+        let p1 = get(apiUrl + "cts/epas").then(r => epas.value = r.result)
+        let p2 = get(apiUrl + "cts/levels").then(r => levels.value = r.result)
+        let p3 = get(apiUrl + "people/" + studentUserId).then(r => person.value = r.result)
+        let p4 = getAssessments()
+        await Promise.all([p1, p2, p3, p4])
+
+        for(let i = 0; i < epas.value.length; i++) {
+            showDetails.value[i] = false
+        }
+        loaded.value = true
+        $q.loading.hide()
     }
 
     async function getAssessments() {
-        //check for url param specifying id of student to get assessments for
-        //for id other than the logged in user, check permission (though server should return 403 if they don't have access)
-        const route = useRoute()
-        const params = route.query
-
-        let studentUserId = userStore.userInfo.userId
-        if (params.student != null) {
-            const studentParam = parseInt(params.student.toString())
-            if (studentParam != undefined && userStore.userInfo.permissions.includes("SVMSecure.CTS.StudentAssessments")) {
-                studentUserId = studentParam
-            }
-        }
-
-        const r = await get(assessmentURL + "?studentUserId=" + studentUserId)
+        const r = await get(apiUrl + "cts/assessments?studentUserId=" + studentUserId)
         epaAssessments.value = r.result
             .sort((e1: Assessment, e2: Assessment) => {
                 if (e1.epaName == null || e2.epaName == null || e1.epaName == e2.epaName) {
@@ -66,84 +70,73 @@
         return epaAssessments.value.filter(e => e.epaId == epaId)
     }
 
-    getEpas()
-    getAssessments()
-
-    /* Chart */
-    function getEpaData(epaId: number | null) {
-        const datasets: ChartData<'line', { key: string, value: number }[]> = {
-            datasets: [{
-                data: [],
-                parsing: {
-                    xAxisKey: 'key',
-                    yAxisKey: 'value'
-                }
-            }],
-        };
-
-        const epas = getAssessmentsForEpa(epaId)
-        const data = [] as number[]
-        const labels = [] as string[]
-        epas.forEach(e => {
-            labels.push(formatDate(e.encounterDate.toString()))
-            datasets.datasets[0].data.push({
-                key: formatDate(e.encounterDate.toString()).toString(),
-                value: e.levelValue
-            })
-            data.push(e.levelValue)
-        })
-        //return datasets
-        return {
-            labels: labels,
-            datasets: [
-                {
-                    label: "",
-                    data: data
-                }
-            ],
-        }
+    function getText(date: Date, enteredBy: string, levelName: string, comment: string | null) {
+        return formatDate(date.toString()) + ' ' + enteredBy+ ' ' + levelName + '\n ' + (comment != null ? comment : "")
     }
 
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            y: {
-                suggestedMin: 1,
-                suggestedMax: 5,
-                display: false
+    function toggleExpandAll() {
+        let anyExpanded = false
+        for(let i = 0; i < showDetails.value.length; i++) {
+            if(showDetails.value[i]) {
+                anyExpanded = true
+                break;
             }
         }
+
+        for(let i = 0; i < showDetails.value.length; i++) {
+            showDetails.value[i] = !anyExpanded
+        }
     }
-    ChartJS.register(CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend)
+
+    load()
 </script>
 <template>
-    <div v-if="epaAssessments.length">
-        <h2>Assessments for {{ epaAssessments[0].studentName }}</h2>
+    <div v-if="loaded">
+        <h2>Assessments for <span v-if="showPersonName && person != null">{{ person.firstName }} {{ person.lastName }}</span></h2>
+
         <div class="row">
-            <div class="col-12 col-lg-2"><strong>EPA</strong></div>
-            <div class="col-12 col-lg-4"><strong>Level</strong></div>
-            <div class="col-12 col-lg-6"><strong>Comments</strong></div>
+            <div class="col col-md-11 col-lg-7 q-mr-sm">
+                <h3>Entrustable Professional Activities</h3>
+            </div>
+            <div class="col-1">
+                <q-btn dense color="secondary" :icon="showDetails.find(s => s) != undefined ? 'expand_less' : 'expand_more'" @click="toggleExpandAll()"></q-btn>
+            </div>
         </div>
-        <div v-for="epa in epas" class="row">
-            <div class="col-12 col-lg-2">
+        <div v-for="(epa, index) in epas" class="row q-mt-sm q-pt-sm assessmentGroup">
+            <div class="col col-md-4 col-lg-3 q-mr-sm">
                 {{ epa.name }}
             </div>
-            <div class="col-12 col-lg-4">
-                <div style="position: relative; height:20vh; width:30vh;">
-                    <Line :data="getEpaData(epa.epaId)" :options="options" />
+            <div class="col col-md-7 col-lg-4">
+                <AssessmentBubble :maxValue="5" :value=a.levelValue :text="getText(a.encounterDate, a.enteredByName, a.levelName, a?.comment)"
+                                  v-for="a in getAssessmentsForEpa(epa.epaId)" :type="bubbleType"></AssessmentBubble>
+            </div>
+            <div class="col-1">
+                <q-btn dense :icon="showDetails[index] ? 'expand_less' : 'expand_more'" @click="showDetails[index] = !showDetails[index]"></q-btn>
+            </div>
+            <q-slide-transition>
+                <div class="col-12 q-mb-md" v-if="showDetails[index]" :key="'epadetails' + index">
+                    <div v-for="a in getAssessmentsForEpa(epa.epaId)" class="row q-mb-sm">
+                        <div class="col-2 col-sm-1">
+                            <AssessmentBubble :maxValue="5" :value=a.levelValue></AssessmentBubble>
+                        </div>
+                        <div class="col-10 col-sm-5 col-md-3 offset-md-0 col-lg-2">
+                            {{ formatDate(a.encounterDate.toString()) }}
+                            {{ a.enteredByName }}
+                        </div>
+                        <div class="col-10 offset-2 col-sm-5 offset-sm-1 offset-md-0 col-md-3 col-lg-2">
+                            {{ a.levelName }}
+                        </div>
+                        <div class="col-10 offset-2 offset-sm-1 offset-md-0 col-md-4 col-lg-3">
+                            {{ a.comment }}
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-12 col-lg-6">
-                <q-expansion-item expand-separator label="View Comments" dense class="q-pa-none" header-class="q-pa-none">
-                    <span v-for="ea in getAssessmentsForEpa(epa.epaId)">
-                        <strong>{{ formatDate(ea.encounterDate.toString()) }} - {{ ea.enteredByName }}</strong>
-                        <br />
-                        {{ ea.comment }}
-                        <br />
-                    </span>
-                </q-expansion-item>
-            </div>
+            </q-slide-transition>
         </div>
+
+        <q-btn-toggle v-model="bubbleType" push toggle-color="primary" no-caps
+                      :options="[{value: 'bubble', 'label': 'Bubbles'}, {value: 'clock', label: 'Progress'}]">
+
+        </q-btn-toggle>
     </div>
 </template>
---
+++ web/Areas/CTS/Controllers/PermissionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Viper.Areas.CTS.Services;
+using Viper.Classes;
+using Viper.Classes.SQLContext;
+using Web.Authorization;
+
+namespace Viper.Areas.CTS.Controllers
+{
+    [Route("/api/cts/permissions/")]
+    [Permission(Allow = "SVMSecure")]
+    public class PermissionsController : ApiController
+    {
+        private readonly CtsSecurityService ctsSecurityService;
+        private readonly RAPSContext _rapsContext;
+        private readonly VIPERContext _viperContext;
+
+        public PermissionsController(VIPERContext context, RAPSContext rapsContext)
+        {
+            _viperContext = context;
+            _rapsContext = rapsContext;
+            ctsSecurityService = new CtsSecurityService(rapsContext, _viperContext);
+        }
+
+        [HttpGet]
+        public ActionResult<bool> HasAccess(string access, int studentId)
+        {
+            switch(access)
+            {
+                case "ViewStudentAssessments":
+                    return ctsSecurityService.CheckStudentAssessmentViewAccess(studentId);
+            }
+            return false;
+        }
+    }
+}
