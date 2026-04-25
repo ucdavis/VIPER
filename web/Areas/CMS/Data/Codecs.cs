1b3e244acc3b9339aefdb276f0d414f36c91e40c
Rex Lorenzo <rexlorenzo@gmail.com>
2026-04-24 17:31:44 -0700

    refactor(cms): collapse Codecs UU/XX encoders onto shared map helpers
    The four public methods now delegate to private EncodeWithMap /
    DecodeWithMap routines parameterised by the encoding map, eliminating
    the byte-for-byte duplication between the UU and XX variants.

--- web/Areas/CMS/Data/Codecs.cs
+++ web/Areas/CMS/Data/Codecs.cs
@@ -51,164 +51,21 @@ namespace Viper.Areas.CMS.Data
         };
 
         public static void UUDecode(System.IO.Stream input, System.IO.Stream output)
-        {
-            if (input == null)
-                throw new ArgumentNullException(nameof(input));
-
-            if (output == null)
-                throw new ArgumentNullException(nameof(output));
-
-            long len = input.Length;
-            if (len == 0)
-                return;
-
-            long didx = 0;
-            int nextByte = input.ReadByte();
-            while (nextByte >= 0)
-            {
-                // get line length (in number of encoded octets)
-                int line_len = UUDecMap[nextByte];
-
-                // ascii printable to 0-63 and 4-byte to 3-byte conversion
-                long end = didx + line_len;
-                byte A, B, C, D;
-                if (end > 2)
-                {
-                    while (didx < end - 2)
-                    {
-                        A = UUDecMap[input.ReadByte()];
-                        B = UUDecMap[input.ReadByte()];
-                        C = UUDecMap[input.ReadByte()];
-                        D = UUDecMap[input.ReadByte()];
-
-                        output.WriteByte((byte)(((A << 2) & 255) | ((B >> 4) & 3)));
-                        output.WriteByte((byte)(((B << 4) & 255) | ((C >> 2) & 15)));
-                        output.WriteByte((byte)(((C << 6) & 255) | (D & 63)));
-                        didx += 3;
-                    }
-                }
-
-                if (didx < end)
-                {
-                    A = UUDecMap[input.ReadByte()];
-                    B = UUDecMap[input.ReadByte()];
-                    output.WriteByte((byte)(((A << 2) & 255) | ((B >> 4) & 3)));
-                    didx++;
-
-                    if (didx < end)
-                    {
-                        C = UUDecMap[input.ReadByte()];
-                        output.WriteByte((byte)(((B << 4) & 255) | ((C >> 2) & 15)));
-                        didx++;
-                    }
-                }
-
-                // skip padding
-                do
-                {
-                    nextByte = input.ReadByte();
-                }
-                while (nextByte >= 0 && nextByte != '\n' && nextByte != '\r');
-
-                // skip end of line
-                do
-                {
-                    nextByte = input.ReadByte();
-                }
-                while (nextByte >= 0 && (nextByte == '\n' || nextByte == '\r'));
-            }
-        }
+            => DecodeWithMap(input, output, UUDecMap);
 
         public static void UUEncode(System.IO.Stream input, System.IO.Stream output)
-        {
-            if (input == null)
-                throw new ArgumentNullException(nameof(input));
-
-            if (output == null)
-                throw new ArgumentNullException(nameof(output));
-
-            long len = input.Length;
-            if (len == 0)
-                return;
-
-            int sidx = 0;
-            int line_len = 45;
-            byte[] nl = Encoding.ASCII.GetBytes(Environment.NewLine);
-
-            byte A, B, C;
-            // split into lines, adding line-length and line terminator
-            while (sidx + line_len < len)
-            {
-                // line length
-                output.WriteByte(UUEncMap[line_len]);
-
-                // 3-byte to 4-byte conversion + 0-63 to ascii printable conversion
-                for (int end = sidx + line_len; sidx < end; sidx += 3)
-                {
-                    A = (byte)input.ReadByte();
-                    B = (byte)input.ReadByte();
-                    C = (byte)input.ReadByte();
-
-                    output.WriteByte(UUEncMap[(A >> 2) & 63]);
-                    output.WriteByte(UUEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                    output.WriteByte(UUEncMap[(C >> 6) & 3 | (B << 2) & 63]);
-                    output.WriteByte(UUEncMap[C & 63]);
-                }
-
-                // line terminator
-                for (int idx = 0; idx < nl.Length; idx++)
-                    output.WriteByte(nl[idx]);
-            }
-
-            // line length
-            output.WriteByte(UUEncMap[len - sidx]);
-
-            // 3-byte to 4-byte conversion + 0-63 to ascii printable conversion
-            while (sidx + 2 < len)
-            {
-                A = (byte)input.ReadByte();
-                B = (byte)input.ReadByte();
-                C = (byte)input.ReadByte();
-
-                output.WriteByte(UUEncMap[(A >> 2) & 63]);
-                output.WriteByte(UUEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                output.WriteByte(UUEncMap[(C >> 6) & 3 | (B << 2) & 63]);
-                output.WriteByte(UUEncMap[C & 63]);
-                sidx += 3;
-            }
-
-            if (sidx < len - 1)
-            {
-                A = (byte)input.ReadByte();
-                B = (byte)input.ReadByte();
-
-                output.WriteByte(UUEncMap[(A >> 2) & 63]);
-                output.WriteByte(UUEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                output.WriteByte(UUEncMap[(B << 2) & 63]);
-                output.WriteByte(UUEncMap[0]);
-            }
-            else if (sidx < len)
-            {
-                A = (byte)input.ReadByte();
-
-                output.WriteByte(UUEncMap[(A >> 2) & 63]);
-                output.WriteByte(UUEncMap[(A << 4) & 63]);
-                output.WriteByte(UUEncMap[0]);
-                output.WriteByte(UUEncMap[0]);
-            }
-
-            // line terminator
-            for (int idx = 0; idx < nl.Length; idx++)
-                output.WriteByte(nl[idx]);
-        }
+            => EncodeWithMap(input, output, UUEncMap);
 
         public static void XXDecode(System.IO.Stream input, System.IO.Stream output)
-        {
-            if (input == null)
-                throw new ArgumentNullException(nameof(input));
+            => DecodeWithMap(input, output, XXDecMap);
 
-            if (output == null)
-                throw new ArgumentNullException(nameof(output));
+        public static void XXEncode(System.IO.Stream input, System.IO.Stream output)
+            => EncodeWithMap(input, output, XXEncMap);
+
+        private static void DecodeWithMap(System.IO.Stream input, System.IO.Stream output, byte[] decMap)
+        {
+            ArgumentNullException.ThrowIfNull(input);
+            ArgumentNullException.ThrowIfNull(output);
 
             long len = input.Length;
             if (len == 0)
@@ -219,7 +76,7 @@ namespace Viper.Areas.CMS.Data
             while (nextByte >= 0)
             {
                 // get line length (in number of encoded octets)
-                int line_len = XXDecMap[nextByte];
+                int line_len = decMap[nextByte];
 
                 // ascii printable to 0-63 and 4-byte to 3-byte conversion
                 long end = didx + line_len;
@@ -228,10 +85,10 @@ namespace Viper.Areas.CMS.Data
                 {
                     while (didx < end - 2)
                     {
-                        A = XXDecMap[input.ReadByte()];
-                        B = XXDecMap[input.ReadByte()];
-                        C = XXDecMap[input.ReadByte()];
-                        D = XXDecMap[input.ReadByte()];
+                        A = decMap[input.ReadByte()];
+                        B = decMap[input.ReadByte()];
+                        C = decMap[input.ReadByte()];
+                        D = decMap[input.ReadByte()];
 
                         output.WriteByte((byte)(((A << 2) & 255) | ((B >> 4) & 3)));
                         output.WriteByte((byte)(((B << 4) & 255) | ((C >> 2) & 15)));
@@ -242,14 +99,14 @@ namespace Viper.Areas.CMS.Data
 
                 if (didx < end)
                 {
-                    A = XXDecMap[input.ReadByte()];
-                    B = XXDecMap[input.ReadByte()];
+                    A = decMap[input.ReadByte()];
+                    B = decMap[input.ReadByte()];
                     output.WriteByte((byte)(((A << 2) & 255) | ((B >> 4) & 3)));
                     didx++;
 
                     if (didx < end)
                     {
-                        C = XXDecMap[input.ReadByte()];
+                        C = decMap[input.ReadByte()];
                         output.WriteByte((byte)(((B << 4) & 255) | ((C >> 2) & 15)));
                         didx++;
                     }
@@ -271,13 +128,10 @@ namespace Viper.Areas.CMS.Data
             }
         }
 
-        public static void XXEncode(System.IO.Stream input, System.IO.Stream output)
+        private static void EncodeWithMap(System.IO.Stream input, System.IO.Stream output, byte[] encMap)
         {
-            if (input == null)
-                throw new ArgumentNullException(nameof(input));
-
-            if (output == null)
-                throw new ArgumentNullException(nameof(output));
+            ArgumentNullException.ThrowIfNull(input);
+            ArgumentNullException.ThrowIfNull(output);
 
             long len = input.Length;
             if (len == 0)
@@ -292,7 +146,7 @@ namespace Viper.Areas.CMS.Data
             while (sidx + line_len < len)
             {
                 // line length
-                output.WriteByte(XXEncMap[line_len]);
+                output.WriteByte(encMap[line_len]);
 
                 // 3-byte to 4-byte conversion + 0-63 to ascii printable conversion
                 for (int end = sidx + line_len; sidx < end; sidx += 3)
@@ -301,10 +155,10 @@ namespace Viper.Areas.CMS.Data
                     B = (byte)input.ReadByte();
                     C = (byte)input.ReadByte();
 
-                    output.WriteByte(XXEncMap[(A >> 2) & 63]);
-                    output.WriteByte(XXEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                    output.WriteByte(XXEncMap[(C >> 6) & 3 | (B << 2) & 63]);
-                    output.WriteByte(XXEncMap[C & 63]);
+                    output.WriteByte(encMap[(A >> 2) & 63]);
+                    output.WriteByte(encMap[(B >> 4) & 15 | (A << 4) & 63]);
+                    output.WriteByte(encMap[(C >> 6) & 3 | (B << 2) & 63]);
+                    output.WriteByte(encMap[C & 63]);
                 }
 
                 // line terminator
@@ -313,7 +167,7 @@ namespace Viper.Areas.CMS.Data
             }
 
             // line length
-            output.WriteByte(XXEncMap[len - sidx]);
+            output.WriteByte(encMap[len - sidx]);
 
             // 3-byte to 4-byte conversion + 0-63 to ascii printable conversion
             while (sidx + 2 < len)
@@ -322,10 +176,10 @@ namespace Viper.Areas.CMS.Data
                 B = (byte)input.ReadByte();
                 C = (byte)input.ReadByte();
 
-                output.WriteByte(XXEncMap[(A >> 2) & 63]);
-                output.WriteByte(XXEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                output.WriteByte(XXEncMap[(C >> 6) & 3 | (B << 2) & 63]);
-                output.WriteByte(XXEncMap[C & 63]);
+                output.WriteByte(encMap[(A >> 2) & 63]);
+                output.WriteByte(encMap[(B >> 4) & 15 | (A << 4) & 63]);
+                output.WriteByte(encMap[(C >> 6) & 3 | (B << 2) & 63]);
+                output.WriteByte(encMap[C & 63]);
                 sidx += 3;
             }
 
@@ -334,19 +188,19 @@ namespace Viper.Areas.CMS.Data
                 A = (byte)input.ReadByte();
                 B = (byte)input.ReadByte();
 
-                output.WriteByte(XXEncMap[(A >> 2) & 63]);
-                output.WriteByte(XXEncMap[(B >> 4) & 15 | (A << 4) & 63]);
-                output.WriteByte(XXEncMap[(B << 2) & 63]);
-                output.WriteByte(XXEncMap[0]);
+                output.WriteByte(encMap[(A >> 2) & 63]);
+                output.WriteByte(encMap[(B >> 4) & 15 | (A << 4) & 63]);
+                output.WriteByte(encMap[(B << 2) & 63]);
+                output.WriteByte(encMap[0]);
             }
             else if (sidx < len)
             {
                 A = (byte)input.ReadByte();
 
-                output.WriteByte(XXEncMap[(A >> 2) & 63]);
-                output.WriteByte(XXEncMap[(A << 4) & 63]);
-                output.WriteByte(XXEncMap[0]);
-                output.WriteByte(XXEncMap[0]);
+                output.WriteByte(encMap[(A >> 2) & 63]);
+                output.WriteByte(encMap[(A << 4) & 63]);
+                output.WriteByte(encMap[0]);
+                output.WriteByte(encMap[0]);
             }
 
             // line terminator
